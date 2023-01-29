7a82a0a0024c6f7180b603c4851fd171102179c7
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-01-28 21:34:17 -0300

    finished exercise

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -7,7 +7,6 @@ const accessAuthAdmin = require("../middlewares/accessAuthAdmin");
 router.get("/", HomeController.showHome);
 router.get("/register", HomeController.showRegister);
 router.get("/login", HomeController.showLogin);
-
 router.get("/admin", accessAuthAdmin, HomeController.showAdminPage);
 
 
